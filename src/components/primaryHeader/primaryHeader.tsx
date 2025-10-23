6d04d70577176cc59fc22a30a07982c52e25a97c
Bus-sen <snbusra66@gmail.com>
2025-10-23 22:14:21 +0300

    primary header updated

--- src/components/primaryHeader/primaryHeader.tsx
+++ src/components/primaryHeader/primaryHeader.tsx
@@ -1,12 +1,9 @@
 ﻿import styles from "./primaryHeader.module.scss";
 import {FaRegUserCircle} from "react-icons/fa";
 import { TbHomeCheck } from "react-icons/tb";
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function PrimaryHeader() {
-    const [menuOpen, setMenuOpen] = useState(false);
-
     return (
         <header>
             <div className={styles.primaryHeader}>
@@ -22,65 +19,23 @@ function PrimaryHeader() {
                 </nav>
 
                 {/* Logo */}
-                <div className={styles.logo}>
-                    <img src="/rezilla-logo.svg" alt="Rezilla logo"/>
+                <NavLink to="/" className={styles.logo}>
+                    <img src="/rezilla-logo.svg" alt="Rezilla logo" />
                     <span>Rezilla</span>
-                </div>
+                </NavLink>
 
                 {/* Right Menu */}
                 <div className={styles.navRight}>
-                    <a href="/login" className={styles.login}>
+                    <NavLink to="/login" className={styles.login}>
                         <FaRegUserCircle size={24} color="black" />
                         Login/Register
-                    </a>
+                    </NavLink>
                     <button className={styles.addListing}>
                         <TbHomeCheck size={24} />
                         Add Listing
                     </button>
                 </div>
-
-                {/* Hamburger */}
-                <button
-                    className={styles.hamburger}
-                    onClick={() => setMenuOpen(!menuOpen)}
-                    aria-expanded={menuOpen}
-                    aria-controls="primary-navigation"
-                    aria-label="Toggle menu"
-                >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
             </div>
-
-            {/* Mobile Menu */}
-            <nav
-                className={`${styles.mobileMenu} ${menuOpen ? styles.active : ""}`}>
-                <button
-                    className={styles.closeButton}
-                    onClick={() => setMenuOpen(false)}
-                    aria-label="Close menu"
-                >
-                    ✕
-                </button>
-                <ul>
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/listings">Listings</NavLink></li>
-                    <li><NavLink to="/services">Services</NavLink></li>
-                    <li><NavLink to="/blogs">Blogs</NavLink></li>
-                    <li>
-                        <a href="/login" className={styles.login}>
-                            <FaRegUserCircle size={24} color="black" /> Login/Register
-                        </a>
-                    </li>
-                    <li>
-                        <button className={styles.addListing}>
-                            <TbHomeCheck size={24} /> Add Listing
-                        </button>
-                    </li>
-                </ul>
-            </nav>
         </header>
     );
 }
